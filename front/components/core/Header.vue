d7e9754fe2cb035227e08dfe13f04555b2859a09
majed samyal <majed.samyal@gmail.com>
2020-05-03 18:13:06 +0530

    layout setup

---
+++ front/components/core/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>Title</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+ header{
+   background: lightgreen;
+   padding: 10px;
+ }
+ h1{
+   color: #222;
+   text-align: center;
+
+ }
+</style>
